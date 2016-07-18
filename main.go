ac60967afe52f4de4ac2044bf65f062f75a3774f
kamono-mafuyu <kamono-mafuyu@dmm.com>
2016-07-18 16:22:28 +0900

    Add pre getHistoryList API

--- main.go
+++ main.go
@@ -3,13 +3,58 @@ package main
 import (
 	"github.com/labstack/echo"
 	"net/http"
-	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/engine/fasthttp"
+)
+
+type (
+	history struct {
+		Number int `json:"number"`
+		Url        string `json:"url"`
+		SendUser string `json:"send_user"`
+		TotalLike int `json:"tatl_like"`
+	}
+
+	responseHistoryList struct {
+		HistoryList []history `json:"history_list"`
+	}
+
 )
 
 func main() {
+	router := Init()
+	router.Run(fasthttp.New(":8888"))
+}
+
+func Init() *echo.Echo {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+
+	// Routes
+	v1 := e.Group("/jiminy/v1")
+	{
+		v1.GET("/history/list/:size", getHistoryList)
+	}
+	return e
+}
+
+// 投稿履歴取得
+func getHistoryList(c echo.Context) error {
+	var historyList []history
+
+	historyList = append(historyList, history{
+		Number: 1,
+		Url: "3exsRhw3xt8",
+		SendUser: "kamono",
+		TotalLike: 5,
 	})
-	e.Run(standard.New(":1323"))
+	historyList = append(historyList, history{
+		Number: 2,
+		Url: "STg4Ya8bEFo",
+		SendUser: "nakamura",
+		TotalLike: 15,
+	})
+
+	response := new(responseHistoryList)
+	response.HistoryList = historyList
+
+	return c.JSON(http.StatusOK, response)
 }
